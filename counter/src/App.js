9b41ceead41b9b41fbe89990e3f2c5fc19631322
Sandipan19000 <sarkarsandipan19000@gmai.com>
2023-08-09 23:50:00 +0530

    counter + & - & reset

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,10 +1,42 @@
-import './App.css';
-import {Button} from '@mui/material';
+import { useState } from "react";
+import "./App.css";
+import { Button, Paper } from "@mui/material";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const incHandler = () => {
+    setCount(count + 1);
+  };
+  const decHandler = () => {
+    setCount(count - 1);
+  };
+  const resetHandler = () => {
+    setCount(0);
+  };
+
   return (
-    <div>
-      
+    <div className="App">
+      <h2 sx={{ fontWeight: "bold" }}> Increament & Decreament </h2>
+      <div className="inc-dec-div">
+        <Button variant="contained" onClick={decHandler}>
+          -
+        </Button>
+        <Paper elevation={3} sx={{
+            minWidth: "80px",
+            marginLeft: "5px",
+            marginRight: "5px",
+            color:'red',
+            fontWeight:'bold',
+            textAlign:'center'
+          }}>{count}</Paper>
+        <Button variant="contained" onClick={incHandler}>
+          +
+        </Button>
+      </div>
+      <Button variant="contained" onClick={resetHandler}>
+        Reset
+      </Button>
     </div>
   );
 }
